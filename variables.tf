6242fcce56f2663fcf2300bc522b3fe5fbf1abc6
Prathamesh <prathameshsmore05.com>
2024-12-30 11:31:05 +0000

    final

--- variables.tf
+++ variables.tf
@@ -15,11 +15,9 @@ variable "debian-instance-type" {
 }
 
 variable "aws_access_key" {
-  description = "The AWS access key for authentication"
-  type        = string
+
 }
 
 variable "aws_secret_key" {
-  description = "The AWS secret key for authentication"
-  type        = string
+
 }
